de3d02a8b69f9cf72b349ef2c962d27106cf3e3c
Bill Watts <bwatts@modeln.com>
2024-05-18 10:00:00 -0700

    initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["sd_torch_auto_*"],
+} satisfies Config;
